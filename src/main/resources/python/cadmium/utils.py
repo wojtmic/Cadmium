b62bdde9c909cf8373ebd2813398b99a3ac66e7a
Wojtmic <mail@wojtmic.dev>
2026-07-04 10:53:41 +0200

    feat: entity utils

--- src/main/resources/python/cadmium/utils.py
+++ src/main/resources/python/cadmium/utils.py
@@ -1,6 +1,7 @@
 import java
 from datetime import datetime
 from cadmium.player import Player
+from cadmium.entity import entity_from_raw
 
 Date = java.type("java.util.Date")
 
@@ -27,4 +28,21 @@ def find_player(name: str):
     raw = _Bukkit.getPlayerExact(name)
     if raw is None:
         return None
+
     return Player(raw=raw)
+
+def get_all_entities_of_type(entity_type=None) -> list:
+    _Bukkit = java.type("org.bukkit.Bukkit")
+    results = []
+    for world in _Bukkit.getWorlds():
+        for raw in world.getEntities():
+            if entity_type is None or raw.getType() == entity_type:
+                results.append(entity_from_raw(raw))
+    return results
+
+def get_all_entities() -> list:
+    return get_all_entities_of_type(None)
+
+def get_all_players() -> list:
+    _Bukkit = java.type("org.bukkit.Bukkit")
+    return [Player(raw=p) for p in _Bukkit.getOnlinePlayers()]
